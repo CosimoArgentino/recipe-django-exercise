29e7d2bbb4405ee1c499ee3abe49fe6ec1ba2c3f
Cosimo Argentino <cosimoargentino@gmail.com>
2023-11-12 17:58:51 +0100

    admin user and test added
    Admin user added, just by following the lessons

--- app/core/models.py
+++ app/core/models.py
@@ -8,12 +8,24 @@ from django.contrib.auth.models import (
 
 class UserManager(BaseUserManager):
 
-    def create_user(self, email, password=None, **extra_field):
-        user = self.model(email=self.normalize_email(email), **extra_field)
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 
 class User(AbstractBaseUser, PermissionsMixin):
