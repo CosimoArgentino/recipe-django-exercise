ca7fd2eece587321c8198a25e6dcedab6b13f05d
Cosimo Argentino <cosimoargentino@gmail.com>
2023-11-12 17:08:48 +0100

    fix test
    fix test

--- app/core/tests/test_model.py
+++ app/core/tests/test_model.py
@@ -19,7 +19,7 @@ class ModelTest(TestCase):
 
         sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
-            ['Test2Example.com', 'Test2@example.com'],
+            ['Test2@Example.com', 'Test2@example.com'],
         ]
 
         for email, expected in sample_emails:
