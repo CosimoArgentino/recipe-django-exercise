b3062ccbc9c1a9dc867739239bb950c87fb715cb
Cosimo Argentino <cosimoargentino@gmail.com>
2023-11-15 15:15:57 +0100

    useless tests removed
    useless tests removed

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,13 +1,9 @@
 from rest_framework import viewsets
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
 from core.models import Recipe
 from recipe import serializers
 
 
 class RecipeViewSet(viewsets.ModelViewSet):
-
     serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
 
